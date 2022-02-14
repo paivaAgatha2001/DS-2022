91cae2a07bc0f3d918ea099202c8add41a21500e
Agatha <92937531+paivaAgatha2001@users.noreply.github.com>
2022-02-14 16:59:34 -0300

    Create NumerosInvertidos.java

---
+++ Revisão-Esquenta/NumerosInvertidos.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class NumerosInvertidos {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int num;
+		
+		int inv = 0, i;
+		
+		System.out.println("digite o número: ");
+		num = in.nextInt();
+		
+		while (num != 0) {
+			i = num % 10; 
+			inv = inv * 10 + i;
+			num = num /10;
+		}
+		
+		System.out.println("o número invertido é: "+inv );
+		
+		in.close();
+	}
+}
