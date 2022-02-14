c8bf806e6060e52ae89cd10e535bb5c71ad1ec1f
Agatha <92937531+paivaAgatha2001@users.noreply.github.com>
2022-02-14 16:57:06 -0300

    Create SalarioLiquidocomIf.java

---
+++ Revisão-Esquenta/SalarioLiquidocomIf.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class SalarioLcomif {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		
+		double vHoraAula, nAulaDadas, salarioB, salarioL, descontoInss;
+		
+		System.out.println("Digite o valor das horas de aula: ");
+		vHoraAula = in.nextDouble();
+		System.out.println("Digite o n�mero de aulas dadas no m�s: ");
+		nAulaDadas = in.nextDouble();
+		
+		salarioB = vHoraAula*nAulaDadas;
+		
+		System.out.println("O salario bruto � de:" +salarioB);
+		
+		if (salarioB <= 1212 ) {
+			
+			descontoInss = (salarioB*7.5)/100;
+			salarioL = salarioB - descontoInss;
+			System.out.println("o desconto � de:"+descontoInss);
+			System.out.println("O sal�rio liquido �: "+salarioL);
+			
+		}else if(salarioB >= 1212.01 && salarioB <= 2427.35 ){
+			descontoInss = (salarioB * 9)/100;
+			salarioL = salarioB - descontoInss;
+			System.out.println("o desconto � de:"+descontoInss);
+			System.out.println("O sal�rio liquido �: "+salarioL);
+			
+		}else if (salarioB >= 2427.36 && salarioB <= 3641.03){
+			descontoInss = (salarioB*12)/100;
+			System.out.println("o desconto � de:"+descontoInss);
+			salarioL = salarioB - descontoInss;
+			System.out.println("O sal�rio liquido �: "+salarioL);
+			
+		}else if ( salarioB >= 3641.04 && salarioB <= 7089.22 ) {
+			descontoInss = (salarioB*14)/100;
+			System.out.println("o desconto é de:"+descontoInss);
+			salarioL = salarioB - descontoInss;
+			System.out.println("O salario liquido é: "+salarioL);
+			
+		}else if ( salarioB >= 7087.23 && salarioB <= 12136.79) {
+			descontoInss = (salarioB*14.5)/100;
+			salarioL = salarioB - descontoInss;
+			System.out.println("o desconto � de:"+descontoInss);
+			System.out.println("O sal�rio liquido �: "+salarioL);
+			
+		}else if ( salarioB >= 12136.80 && salarioB <= 24273.57 ) {
+			descontoInss = (salarioB*16.5)/100;
+			salarioL = salarioB - descontoInss;
+			System.out.println("o desconto � de:"+descontoInss);
+			System.out.println("O sal�rio liquido �: "+salarioL);
+			
+		}else if ( salarioB >= 24273.58 && salarioB <= 47333.46 ) {
+			descontoInss = (salarioB*19)/100;
+			salarioL = salarioB - descontoInss;
+			System.out.println("o desconto � de:"+descontoInss);
+			System.out.println("O sal�rio liquido �: "+salarioL);
+			
+		}else {
+			descontoInss = (salarioB*22)/100;
+			salarioL = salarioB - descontoInss;
+			System.out.println("o desconto � de:"+descontoInss);
+			System.out.println("O sal�rio liquido �: "+salarioL);
+		}
+		
+		in.close();
+	}
+}
